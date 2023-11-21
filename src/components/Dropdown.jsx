db5921848263780f4756810d29ae514093418278
MuditRajput2001 <you@example.commuditrajputpersonal@gmail>
2023-11-22 01:09:31 +0530

    third

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,24 +1,50 @@
-import React, { useState } from 'react'
-import { useDispatch } from 'react-redux'
-import { setFilter } from '../store/productSlice'
-const Dropdown = ({product}) => {
-    const[selection,setSeletion] = useState()
-     const handle=(e)=>{
-        setSeletion(e.target.value);
-     }
-    const dispatch = useDispatch();
-    dispatch(setFilter(selection));
-  return (
-    <div className='rounded-md border-2 border-black px-2'>
-        <select name='Filter'  value={selection} onChange={handle}>
-            <option>Filter</option>
-            <option name= "Price high to lower">Price high to lower</option>
-            <option name= "Price lower to higher">Price lower to higher</option>
-            <option name= "Sort(a to z)">Sort(a to z)</option>
-            <option name= "Sort(z to a)">Sort(z to a)</option>
-        </select>
-    </div>
-  )
-}
+// import React, { useState } from 'react'
+// import { useDispatch } from 'react-redux'
+// import { setFilter } from '../store/productSlice'
+// const Dropdown = ({product}) => {
+//     const[selection,setSeletion] = useState()
+//      const handle=(e)=>{
+//         setSeletion(e.target.value);
+//      }
+//     const dispatch = useDispatch();
+//     dispatch(setFilter(selection));
+//   return (
+//     <div className='rounded-md border-2 border-black px-2'>
+//         <select name='Filter'  value={selection} onChange={handle}>
+//             <option>Filter</option>
+//             <option name= "Price high to lower">Price high to lower</option>
+//             <option name= "Price lower to higher">Price lower to higher</option>
+//             <option name= "Sort(a to z)">Sort(a to z)</option>
+//             <option name= "Sort(z to a)">Sort(z to a)</option>
+//         </select>
+//     </div>
+//   )
+// }
 
-export default Dropdown
+// export default Dropdown
+import { useState } from "react";
+
+// Inside Dropdown component
+const Dropdown = ({ product, onFilterChange }) => {
+  const [selection, setSelection] = useState();
+
+  const handle = (e) => {
+      const selectedFilter = e.target.value;
+      setSelection(selectedFilter);
+      onFilterChange(selectedFilter);
+  };
+
+  return (
+      <div className='rounded-md border-2 border-black px-2'>
+          <select name='Filter' value={selection} onChange={handle}>
+              <option>Filter</option>
+              <option value="Price high to lower">Price high to lower</option>
+              <option value="Price lower to higher">Price lower to higher</option>
+              <option value="Sort(a to z)">Sort(a to z)</option>
+              <option value="Sort(z to a)">Sort(z to a)</option>
+          </select>
+      </div>
+  );
+};
+
+export default Dropdown;
--- src/components/RightSidebar.jsx
+++ src/components/RightSidebar.jsx
@@ -1,7 +1,8 @@
-import React from 'react';
+// import React from 'react';
 import { FaListUl } from "react-icons/fa";
 import { FiGrid } from "react-icons/fi";
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { setFilter } from '../store/productSlice';
 import CardLoop from './CardLoop';
 import Dropdown from './Dropdown';
 
@@ -13,13 +14,13 @@ const RightSidebar = () => {
   
     let value =
       categoryFilter === 'men'
-        ? `men's clothing`
+        ? "men's clothing"
         : categoryFilter === 'women'
-        ? `women's clothing`
+        ? "women's clothing"
         : categoryFilter === 'electronics'
         ? 'electronics'
         : categoryFilter === 'jewelry'
-        ? `jewelry`
+        ? 'jewelry'
         : 'All';
   
     const filteredProduct =
@@ -27,42 +28,50 @@ const RightSidebar = () => {
         ? product.filter((product) => product.category === value)
         : product;
   
-    const rangedProduct = filteredProduct.filter(
-      (product) => product.price <= range
-    );
-//   console.log(rangedProduct);
-    // let sortedProduct = [...rangedProduct];
-    // console.log(sortedProduct);
-    if (filterName === 'Price high to low') {
-        rangedProduct.sort((a, b) => b.price - a.price);
-    } else if (filterName === 'Price low to high') {
-        rangedProduct.sort((a, b) => a.price - b.price);
-    } else if (filterName === 'Sort (a to z)') {
-        rangedProduct.sort((a, b) => a.title.localeCompare(b.title));
-    } else if (filterName === 'Sort (z to a)') {
-        rangedProduct.sort((a, b) => b.title.localeCompare(a.title));
+        const rangedProduct = filteredProduct.filter(
+          (product) => product.price <= range
+        );
+        console.log('Range:', range);
+    
+        console.log('Before Price Filter:', filteredProduct);
+    console.log('After Price Filter:', rangedProduct);
+    
+    if (filterName === 'Price high to lower') {
+      rangedProduct.sort((a, b) => b.price - a.price);
+    } else if (filterName === 'Price lower to higher') {
+      rangedProduct.sort((a, b) => a.price - b.price);
+    } else if (filterName === 'Sort(a to z)') {
+      rangedProduct.sort((a, b) => a.title.localeCompare(b.title));
+    } else if (filterName === 'Sort(z to a)') {
+      rangedProduct.sort((a, b) => b.title.localeCompare(a.title));
     }
-  console.log(rangedProduct);
-
-  return (
-    <div>
-      <div className='flex justify-between '>
-        <div className='flex'>
-          <FiGrid />
-          <FaListUl />
-        </div>
-        <div>
-          Number of products : <span>{rangedProduct.length}</span>
-        </div>
-        <div>
-          <Dropdown product={rangedProduct} />
-        </div>
-      </div>
-      <div className='grid grid-cols-3 w-[990px] p-2 gap-3'>
-        <CardLoop product={rangedProduct} />
-      </div>
-    </div>
-  );
-};
-
-export default RightSidebar;
+    const dispatch = useDispatch();
+    console.log(rangedProduct);
+    
+    const handleFilterChange = (selectedFilter) => {
+      dispatch(setFilter(selectedFilter));
+    };
+        console.log('Before Price Filter:', filteredProduct);
+        console.log('After Price Filter:', rangedProduct);
+        return (
+          <div>
+            <div className='flex justify-between '>
+              <div className='flex'>
+                <FiGrid />
+                <FaListUl />
+              </div>
+              <div>
+                Number of products : <span>{rangedProduct.length}</span>
+              </div>
+              <div>
+                <Dropdown product={rangedProduct} onFilterChange={handleFilterChange} />
+              </div>
+            </div>
+            <div className='grid grid-cols-3 w-[990px] p-2 gap-3'>
+              <CardLoop product={rangedProduct} />
+            </div>
+          </div>
+        );
+      };
+      
+      export default RightSidebar;
